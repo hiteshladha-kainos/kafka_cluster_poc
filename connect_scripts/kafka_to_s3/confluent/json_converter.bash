572c08f568214f732f696c7ae10cfb07a3edf025
Ubuntu <ubuntu@ip-172-31-0-86.ec2.internal>
2022-05-11 14:24:52 +0000

    changes for new enfironment

--- connect_scripts/kafka_to_s3/confluent/json_converter.bash
+++ connect_scripts/kafka_to_s3/confluent/json_converter.bash
@@ -6,14 +6,14 @@ curl -X POST \
     "config":
     {
       "connector.class":"io.confluent.connect.s3.S3SinkConnector",
-      "s3.region":"eu-west-1",
+      "s3.region":"us-east-1",
       "flush.size":10,
       "schema.compatibility":"NONE",
       "tasks.max":10,
-      "topics":"subscriptions_confluent",
+      "topics":"subscriptions_ibm",
       "format.class":"io.confluent.connect.s3.format.json.JsonFormat",
       "storage.class":"io.confluent.connect.s3.storage.S3Storage",
-      "s3.bucket.name":"kp-hftestingbucket",
+      "s3.bucket.name":"kafkaclusterpoc",
       "topics.dir":"confluent",
       "key.converter":"org.apache.kafka.connect.storage.StringConverter",
       "value.converter":"org.apache.kafka.connect.json.JsonConverter",
